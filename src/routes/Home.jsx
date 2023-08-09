3a33e345bacd581f673b751828995895751db260
Yeison Garcia <garciamonsefu@gmail.com>
2023-08-09 15:30:51 -0500

    New file with the company's services and update in the services view on the home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,9 @@ import Landing from "../components/Landing";
 import Benefit from "../components/Benefit";
 import Service from "../components/Service";
 
+import { SERVICES } from "../data/services";
 import { COMPANY_BENEFITS } from "../data/benefits";
+// import { COMPANY_BENEFITS, SERVICES  } from "../data";
 
 
 
@@ -29,8 +31,7 @@ export default function Home() {
                                 name={benefit.name}
                                 description={benefit.description}
                                 src={benefit.src} />
-                        ))
-                        }
+                        ))}
                     </div>
                 </div>
             </div>
@@ -44,35 +45,18 @@ export default function Home() {
                     </h2>
 
                     <div className="w-full flex flex-col items-center gap-4 md:flex-row flex-wrap md:w-[720px] md:gap-8 xl:gap-4 justify-center xl:w-full 2xl:gap-10">
-                        <Service
-                            name={'Estimulación Temprana'}
-                            description={'Actividades dirigidas a estimular y potenciar diferentes áreas del desarrollo, como el cognitivo, motor, socio emocional y del lenguaje.'}
-                            src={'/images/services/early-stimulation.png'} />
-                        <Service
-                            name={'Psicología Clínica'}
-                            description={'Rama de la psicología que se centra en la evaluación, diagnóstico, tratamiento de los trastornos mentales y emocionales.'}
-                            src={'/images/services/clinical-psychology.png'} />
-                        <Service
-                            name={'Psicología Educativa'}
-                            description={'Entender cómo las personas aprenden y se desarrollan en contextos educativos, aplicando los principios y métodos de la psicología.'}
-                            src={'/images/services/educational-psychology.png'} />
-
-                        <Service
-                            name={'Diagnóstico Clínico'}
-                            description={'Identificación y clasificación de síntomas que tiene el pacientes como puede ser un trastorno o una condición psicológica.'}
-                            src={'/images/services/clinical-diagnosis.png'} />
-                        <Service
-                            name={'Psicología Organizacional'}
-                            description={'Comprender y mejorar la eficacia, el bienestar y el desarrollo de las personas en el ámbito laboral y organizacional.'}
-                            src={'/images/services/organizational-psychology.png'} />
-                        <Service
-                            name={'Capacitaciones'}
-                            description={'Programas de formación y desarrollo que tienen como objetivo mejorar las habilidades y competencias de las personas.'}
-                            src={'/images/services/trainings.png'} />
-
+                        {SERVICES.map((service, index) => (
+                            <Service
+                                key={index}
+                                name={service.name}
+                                description={service.description}
+                                src={service.src} />
+                        ))}
                     </div>
                 </main>
             </div>
+
+            
 
             <Footer />
         </>
