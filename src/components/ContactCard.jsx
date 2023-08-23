44e9c449221e04b74a109d0c6264da6af52e03b5
Yeison Garcia <garciamonsefu@gmail.com>
2023-08-23 09:51:06 -0500

    Add Google Maps link of the address of the psychological center in the contact card

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -4,9 +4,9 @@ export default function ContactCard() {
             <h2 className="w-full text-2xl md:text-[32px] mb-6 md:mb-8 lg:mb-12 text-center font-bold text-[#EC6A5D]">Contáctanos</h2>
 
             <div className="flex flex-col xl:flex-row-reverse gap-4 md:gap-6 xl:gap-12">
-                <div className="w-full rounded-lg">
+                <a href="https://goo.gl/maps/Wk5qDAfVrwKiDqYa7" target="_blank" className="w-full rounded-lg">
                     <img src="/images/map.webp" alt="mapa" loading="lazy" className="w-full max-h-[310px] lg:max-h-[420px] object-cover rounded-lg" />
-                </div>
+                </a>
 
                 <div className="w-full xl:w-1/3 flex flex-col gap-8 md:gap-12">
                     <div className="flex flex-col gap-4 md:gap-8 lg:gap-9 text-[#00B4E3]">
